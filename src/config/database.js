53017363b83a539dca4de608aaad84e13be046d3
Nikhil Shinde <nikhilshahajishinde1111@.com>
2026-04-19 07:30:09 +0530

    update in DB

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
  export const connectdb=async()=>{
 
-  await  mongoose.connect("mongodb+srv://tukaramkangude13:QwDZoL2OIPHJqL2v@tukaramkangude13.g2bue.mongodb.net/tk")
+  await  mongoose.connect("mongodb+srv://tukaramkangude13:Tukaram1qw23e@tukaramkangude13.g2bue.mongodb.net/")
     // QwDZoL2OIPHJqL2v
 }
 
